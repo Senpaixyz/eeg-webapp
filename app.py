da1783f2732e8e03a571084c727928f52333fd12
Senpaixyz <jhicer@gmail.com>
2022-07-05 02:46:44 +0800

    pull new update to the web app-jhino

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('index.html',
-                           )
+    return render_template('index.html')
+
+
+@app.route('/start', methods=['GET'])
+def start():
+    return render_template('menu.html')
 
 if __name__ == "__main__":
     #app.run(debug=True, host='0.0.0.0', port='5000')
